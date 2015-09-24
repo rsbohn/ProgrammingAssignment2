80d0ba6637e3a566d1469e117f3060c81d868056
Randall Bohn <rsbohn@gmail.com>
2015-09-24 03:42:41 -0600

    Implement the makeCacheMatrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## Randall Bohn 2015-09-24
+## Store a matrix and the inverse of the matrix.
+## Caches the inverse so you only have to SOLVE it once.
 
-## Write a short comment describing this function
+## This is the thing that holds a matrix (mx) 
+## and the inverse (inv).
+## it has getters and setters for each field.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mx = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    mx <<- y
+    inv <<- NULL
+  }
 
+  get <- function() mx
+  setinverse <- function(inv_) inv <<- inv_
+  getinverse <- function() inv
+
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
